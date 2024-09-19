7ee4f76b39bdb6a206563c3148f19c6cf44f4cce
g0al <goalenator@gmail.com>
2024-09-20 01:14:00 +0300

    error fixed

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -6,32 +6,9 @@ import java.util.Scanner;
 
 public class Progression {
 
-    public static String correctAnswer;
-
     public static void startGame() {
 
         Scanner scanner = new Scanner(System.in);
-
-        // greeting and asking user
-        Engine.userGreeting();
-        System.out.println("What number is missing in the progression?");
-
-        // the mechanism of the game
-        for (int i = 0; i < Engine.getGameAttempts(); i++) {
-
-            createAndShowProgression();
-
-            // getting answer from user
-            String userAnswer = scanner.nextLine();
-            System.out.println("Your answer: " + userAnswer);
-
-            // checking answer
-            Engine.checkAnswer(userAnswer, correctAnswer);
-        }
-        System.out.println("Congratulations, " + Engine.getUserName() + "!");
-    }
-
-    public static void createAndShowProgression() {
 
         // determine the parameters of the progression
         final int minNumbersInProgression = 5;
@@ -43,31 +20,48 @@ public class Progression {
         final int minStep = 2;
         final int maxStep = 5;
 
-        // progression parameters
-        int numbersInProgression = Engine.generateRandomNumber(minNumbersInProgression,
-                maxNumbersInProgression);
-        int firstNumber = Engine.generateRandomNumber(minFirstNumber, maxFirstNumber);
-        int step = Engine.generateRandomNumber(minStep, maxStep);
-        int missingNumberIndex = Engine.generateRandomNumber(0, numbersInProgression - 1);
+        // greeting and asking user
+        Engine.userGreeting();
+        System.out.println("What number is missing in the progression?");
 
-        // creating and filling array with numbers
-        int[] numbers = new int[numbersInProgression];
-        numbers[0] = firstNumber;
-        for (int k = 1; k < numbers.length; k++) {
-            numbers[k] = numbers[k - 1] + step;
-        }
+        // the mechanism of the game
+        for (int i = 0; i < Engine.getGameAttempts(); i++) {
 
-        // making a string for user
-        System.out.print("Question: ");
-        for (int j = 0; j < numbers.length; j++) {
-            if (j != missingNumberIndex) {
-                System.out.print(numbers[j] + " ");
-            } else {
-                System.out.print(".. ");
+            // progression parameters
+            int numbersInProgression = Engine.generateRandomNumber(minNumbersInProgression,
+                    maxNumbersInProgression);
+            int firstNumber = Engine.generateRandomNumber(minFirstNumber, maxFirstNumber);
+            int step = Engine.generateRandomNumber(minStep, maxStep);
+            int missingNumberIndex = Engine.generateRandomNumber(0, numbersInProgression - 1);
+
+            // creating and filling array with numbers
+            int[] numbers = new int[numbersInProgression];
+            numbers[0] = firstNumber;
+            for (int k = 1; k < numbers.length; k++) {
+                numbers[k] = numbers[k - 1] + step;
             }
-        }
-        System.out.println();
 
-        correctAnswer = Integer.toString(numbers[missingNumberIndex]);
+            // making a string for user
+            System.out.print("Question: ");
+            for (int j = 0; j < numbers.length; j++) {
+                if (j != missingNumberIndex) {
+                    System.out.print(numbers[j] + " ");
+                } else {
+                    System.out.print(".. ");
+                }
+            }
+            System.out.println();
+
+            // getting answer from user
+            String userAnswer = scanner.nextLine();
+            System.out.println("Your answer: " + userAnswer);
+
+            // correct answer
+            var correctAnswer = Integer.toString(numbers[missingNumberIndex]);
+
+            // checking answer
+            Engine.checkAnswer(userAnswer, correctAnswer);
+        }
+        System.out.println("Congratulations, " + Engine.getUserName() + "!");
     }
 }
